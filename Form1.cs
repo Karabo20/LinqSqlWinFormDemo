28b918d0b856daaba018c50023e6640688adcd4c
Karabo Molema <mrkaymp@gmail.com>
2024-12-03 10:11:48 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+`
+namespace LinqSqlWinFormDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DataClasses1DataContext data = new DataClasses1DataContext();
+            var results = from workers in data.Peoples
+                          where workers.Name == "Donald"
+                          select workers;
+
+
+            dataGridView1.DataSource = results;
+        }
+    }
+}
